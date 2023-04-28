3b08a973d56e03bcdc4c60fef590961bd5ff2a03
BI TAH DIDIER TAH <didier.tah@mqashgroup.com>
2023-04-28 10:36:45 +0000

    two

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './screens/WelcomeScreen.js';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
-    <ScrollView>
-      <WelcomeScreen/>
-    </ScrollView>
+   
+      
+      <HomeScreen/>
+   
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
